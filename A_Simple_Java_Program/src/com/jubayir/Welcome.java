4c1cb54fb5df09b7700cdfbd751aa6be5e1e3946
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-23 11:17:50 +0600

    Simple_Java_Program

---
+++ A_Simple_Java_Program/src/com/jubayir/Welcome.java
@@ -0,0 +1,11 @@
+package com.jubayir;
+
+public class Welcome {
+
+    public static void main(String[] args) {
+        // Display message Welcome to Java! on the console
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Md. Jubayir Hossain Program!");
+    }
+
+}
