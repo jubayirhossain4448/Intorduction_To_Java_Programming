58826dbb0450c4ba39b742651fde9d88ae41f58d
jubayirhossain4448 <jubayirhossain4448@gmail.com>
2019-06-24 23:11:20 +0600

    Compute_Expression

--- A_Simple_Java_Program/src/com/jubayir/Welcome.java
+++ A_Simple_Java_Program/src/com/jubayir/Welcome.java
@@ -1,11 +1,15 @@
 package com.jubayir;
 
-public class Welcome {
+public class Welcome {  // Class block
 
-    public static void main(String[] args) {
+    public static void main(String[] args) {  // Method block
         // Display message Welcome to Java! on the console
         System.out.println("Welcome to Java!");
         System.out.println("Welcome to Md. Jubayir Hossain Program!");
     }
 
 }
+
+// Every Java program must have at least one class.
+// Class name statrs with uppercase letter.
+
---
+++ ComputeExpression/src/com/jubayir/ComputeExpression.java
@@ -0,0 +1,8 @@
+package com.jubayir;
+
+public class ComputeExpression {
+    public static void main(String[] args) {
+        System.out.println("Example of Compute Expression: " + ((10.5 + 2 * 3) / (45-3.5)));
+    }
+    
+}
